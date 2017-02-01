ad9103d710d8d75aa9e618e1438fd2dd12d21c52
anang.bakti <anang.bakti@ptdmc.co.id>
2017-02-01 22:24:12 +0700

    Add entity player and emoticons

--- Game2/Game1.cs
+++ Game2/Game1.cs
@@ -13,10 +13,12 @@ namespace SnakeLadderQuiz.Desktop
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        private Texture2D background;
+        private Texture2D background;        
         private List<Rectangle> rects = new List<Rectangle>();
         private List<Texture2D> textureRects = new List<Texture2D>();
         private Color rectColor;
+
+        private List<Player> players = new List<Player>(5);
 
         public Game1()
         {
@@ -28,6 +30,12 @@ namespace SnakeLadderQuiz.Desktop
             Content.RootDirectory = "Content";
             rectColor = Color.Transparent;
             CreateListOfRectangle();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var player = new Player(i);                
+                players.Add(player);
+            }
             
         }
 
@@ -55,7 +63,16 @@ namespace SnakeLadderQuiz.Desktop
 
             // TODO: use this.Content to load your game content here
             background = Content.Load<Texture2D>("snake_ladder");
-            CreateListOfTextureRect(rectColor);
+                        
+            players[0].Texture2D = Content.Load<Texture2D>("emo_gigikawat");
+            players[1].Texture2D = Content.Load<Texture2D>("emo_melet");
+            players[2].Texture2D = Content.Load<Texture2D>("emo_kacagigi");
+            players[3].Texture2D = Content.Load<Texture2D>("emo_nangis");
+            players[4].Texture2D = Content.Load<Texture2D>("emo_duit");
+
+            CreateListOfTextureRect();
+
+            initFirstMoveAllPlayer();
         }
 
         /// <summary>
@@ -77,8 +94,21 @@ namespace SnakeLadderQuiz.Desktop
         protected override void Update(GameTime gameTime)
         {
             // TODO: Add your update logic here
+            
+            // Allows the game to exit
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            //    this.Exit();
+            
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Enter))
+            {
+                // do something here
+                // sample move
+                players[0].Vector2 = new Vector2(rects[10].X, rects[10].Y);
+            }
 
             System.GC.Collect();
+            
             base.Update(gameTime);
         }
 
@@ -93,7 +123,8 @@ namespace SnakeLadderQuiz.Desktop
             // TODO: Add your drawing code here            
             spriteBatch.Begin();
             spriteBatch.Draw(background, new Rectangle(0, 0, 800, 600), Color.White);
-            DrawEachBoxInSnakeLadder(rectColor);
+            DrawEachBoxInSnakeLadder();
+            DrawEachPlayer();
             spriteBatch.End();
 
             base.Draw(gameTime);
@@ -114,6 +145,7 @@ namespace SnakeLadderQuiz.Desktop
             for (int i = 1; i < 100; i++)
             {
 
+                // I know var keAtas can set in one line, but I want it readable
                 if (i % 10 == 0)
                 {
                     keAtas = true;
@@ -140,24 +172,91 @@ namespace SnakeLadderQuiz.Desktop
             }
         }
 
-
-        private void CreateListOfTextureRect(Color color)
+        private void CreateListOfTextureRect()
         {
             for (int i =0; i < 100; i++)
             {
                 var textureRect = new Texture2D(this.GraphicsDevice, 1, 1);
-                textureRect.SetData(new[] { color });
+                textureRect.SetData(new[] { rectColor });
                 textureRects.Add(textureRect);
             }
             
         }
 
-        private void DrawEachBoxInSnakeLadder(Color color ) {            
+        private void DrawEachBoxInSnakeLadder() {            
             for (int i = 0; i < 100; i++)
             {
-                spriteBatch.Draw(textureRects[i], rects[i], color);
+                spriteBatch.Draw(textureRects[i], rects[i], rectColor);
             }
            
+        }
+
+        private void DrawEachPlayer() {
+            foreach (var player in players)
+            {
+                spriteBatch.Draw(player.Texture2D, new Rectangle((int)player.Vector2.X, (int)player.Vector2.Y, 30, 30), Color.White);
+            }
+        }
+
+        private void initFirstMoveAllPlayer() {
+            foreach (var player in players)
+            {
+                SetPositionPlayer(rects[0], player);
+            }
+        }
+
+        private void SetPositionPlayer(Rectangle rect, Player player)
+        {
+            if (player.Id == 0)
+            {
+                player.Vector2 = new Vector2(rect.X, rect.Y);
+            }
+            else if (player.Id == 1)
+            {
+                player.Vector2 = new Vector2(rect.X+40, rect.Y);
+            }
+            else if (player.Id == 2)
+            {
+                player.Vector2 = new Vector2(rect.X, rect.Y+22);
+            }
+            else if (player.Id == 3)
+            {
+                player.Vector2 = new Vector2(rect.X+40, rect.Y+22);
+            }
+            else if (player.Id == 4)
+            {
+                player.Vector2 = new Vector2(rect.X+20, rect.Y+11);
+            }
+        }
+
+        public class Player
+        {
+
+            public Player(int Id)
+            {
+                Position = 0;
+                PositionDestination = 0;
+                PlayerMode = Player.Mode.Stop;
+                this.Id = Id;
+            }
+
+            public int Id { get; set; }
+
+            public enum Mode
+            {
+                Stop,
+                Walking
+            }
+
+            public Texture2D Texture2D { get; set; }
+            public Vector2 Vector2 { get; set; }
+            public int Position { get; set; }
+            public Mode PlayerMode { get; set; }
+            public int PositionDestination { get; set; }
+            public bool IsInPositionDestination()
+            {
+                return Position == PositionDestination;
+            }
         }
 
     }
